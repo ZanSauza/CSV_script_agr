4095f002b245aa6bfab1600c04bbbae931a43538
Sauza <fedyai@gmail.com>
2025-07-03 17:35:30 +0300

    qwerty

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import argparse
 from tabulate import tabulate
 
 
+def filter_table(table, column_name, filter_value):
+    try:
+        column_index = table[0].index(column_name)
+    except ValueError:
+        print(f"Ошибка: Столбец с именем '{column_name}' не найден в таблице.")
+        return []
+
+    filtered_rows = [row for row in table[1:] if row[column_index] == filter_value]
+    return filtered_rows
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--file', type=str)
@@ -21,7 +32,14 @@ def main():
         for i in content:
             data.append(i.split(";"))
 
-    print(tabulate(arg_filter if arg_filter in data else "None"))
+    filtered_rows = filter_table(data, args.where)
+
+    if filtered_rows:
+        print("Результаты фильтрации:")
+        for row in filtered_rows:
+            print(row)
+
+    # print(tabulate(arg_filter if arg_filter in data else "None"))
 
         # print(tabulate(data, headers="firstrow", tablefmt="grid"))
 
